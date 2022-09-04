3a7491433afa6d01ac6fc51acffd01d8ab6279e3
Lena <len.dal@yandex.ru>
2022-09-04 20:15:51 +0300

    second task

--- second_task.py
+++ second_task.py
@@ -6,7 +6,7 @@ print('x y z')
 for x in range(2):
   for y in range(2):
     for z in range(2):
-        print(x, y, z)
+        print(x, y, z, end=" ")
         if (not (x or y or z) == (not x and not y and not z)):
             print ("True")
         else:
