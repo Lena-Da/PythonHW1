e452aafd2f482615ec062e6f9f2bc503ba1b6ee0
Lena <len.dal@yandex.ru>
2022-09-04 20:10:13 +0300

    second task

---
+++ second_task.py
@@ -0,0 +1,13 @@
+#Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат. 
+#Предикату можно заменить на понятие "бит".
+#Должна получиться таблица истинности.
+
+print('x y z')
+for x in range(2):
+  for y in range(2):
+    for z in range(2):
+        print(x, y, z)
+        if (not (x or y or z) == (not x and not y and not z)):
+            print ("True")
+        else:
+            print ("False")
